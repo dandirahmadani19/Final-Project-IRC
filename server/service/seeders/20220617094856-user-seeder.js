65528516d7749add32b4182ca238c6a2f88f7477
Romzi Fauzi <romziflubis@gmail.com>
2022-06-17 17:03:31 +0700

    add user and admin seeder

---
+++ server/service/seeders/20220617094856-user-seeder.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'jhon@mail.com',
+          password: '123456',
+          phoneNumber: '081234567890',
+          address: 'Jl. Kebon Kacang',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {
+      truncate: true,
+      cascade: true,
+      restartIdentity: true,
+    });
+  },
+};
---
+++ server/service/seeders/20220617095548-admin-seeder.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert(
+      'Admins',
+      [
+        {
+          username: 'admin',
+          email: 'admin@mail.com',
+          password: 'admin123',
+          phoneNumber: '081234567890',
+          address: 'Jl. Kebon Kacang',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Admins', null, {
+      truncate: true,
+      cascade: true,
+      restartIdentity: true,
+    });
+  },
+};
