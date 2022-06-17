6a0ce96e5813f1abced93d3ea43a0f73e3ac6e4c
dandirahmadani19 <dandirahmadani19@gmail.com>
2022-06-17 13:49:56 +0700

    finished home screen

--- client-mobile/App.js
+++ client-mobile/App.js
@@ -1,24 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./app/screens/HomeScreen";
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <HomeScreen />
-    </>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
